f974c1fb49849194db15cc47afd7f1517ef8ddbc
ronak5487 <20bee055@nith.ac.in>
2023-06-24 02:25:40 +0530

    clien id fixed

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(passport.session());
 
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: process.env.CLIENT_URL,
     methods: 'GET,PUT,POST,DELETE',
     credentials: true
 }));
